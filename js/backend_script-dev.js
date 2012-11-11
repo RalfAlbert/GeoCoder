125186386bb03dd28889b57f584d30010afcd993
Ralf Albert <neun12@gmail.com>
2012-11-11 04:44:05 +0100

    -enhancement: copy the formatted address from google maps geocoder if an convertion will be done to the fields for a proper address display

--- js/backend_script-dev.js
+++ js/backend_script-dev.js
@@ -64,7 +64,7 @@ if( null !== ( jQuery && google ) ){
 			/**
 			 * Switch between converting address-to-latlng and latlng-to-address
 			 *
-			 * @returns	null		Returns null if no data to convert available
+			 * @returns	null	Returns null if no data to convert available
 			 */
 			switchConverter: function(){
 
@@ -153,6 +153,13 @@ if( null !== ( jQuery && google ) ){
 					function( results, status ){
 
 						if( status === google.maps.GeocoderStatus.OK ){
+
+							var data = results[0].formatted_address.split( ",", 3 );
+							var zipcity = $.trim( data[1] ).split( " " );
+
+							gcb.fields.street.val( data[0] );
+							gcb.fields.zip.val( zipcity[0] );
+							gcb.fields.city.val( zipcity[1] );
 
 							gcb.fields.lat.val( results[0].geometry.location.Ya );
 							gcb.fields.lng.val( results[0].geometry.location.Za );
